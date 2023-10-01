4c569b9d917a2702c6cceed054e68664fdf7acd6
obada <opadaalziede@gmail.com>
2023-10-01 10:33:20 +0300

    add command to seed roles to database

---
+++ app/Console/Commands/RoleSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Role;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+
+class RoleSeeder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'role:seed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Role::firstOrCreate(['name' => Config::get('constants.roles.user', 'user_role')]);
+        Role::firstOrCreate(['name' => Config::get('constants.roles.admin', 'admin_role')]);
+
+        $this->info("Roles seeded successfully !");
+
+        return 1;
+    }
+}
---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'roles' => [
+        'user' => 'user_role',
+        'admin' => 'admin_role'
+    ]
+];
