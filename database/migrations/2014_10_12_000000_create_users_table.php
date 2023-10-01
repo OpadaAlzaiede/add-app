83c7ce50a398061d0c942d6d4acdf4f8b3a468ff
obada <opadaalziede@gmail.com>
2023-10-01 10:20:58 +0300

    setup migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('is_activated')->default('0')->comment('0 => not active, 1 => active');
             $table->rememberToken();
             $table->timestamps();
         });
